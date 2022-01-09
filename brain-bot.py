3c018eff49258fb9d922517fb258a4e8588ddb62
masonrware <86129546+masonrware@users.noreply.github.com>
2022-01-08 16:58:14 -0800

    imported packages and brought bot online

--- brain-bot.py
+++ brain-bot.py
@@ -1,5 +1,7 @@
 import discord
+import os
 from discord.ext import commands, tasks
+from dotenv import load_dotenv
 import youtube_dl
 
 from random import choice
@@ -11,5 +13,6 @@ client = commands.Bot(command_prefix='?')
 async def on_ready():
     print('brainBot is now online!')
 
-
-client.run('OTI5MjQ2NTYwOTkzMzEyODYw.YdkiTA.b8nSqb6MTnt3oJQ5n615ufVFgiY')
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+client.run(TOKEN)
