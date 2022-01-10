f7cec8384b5f3e542f4d318ef51c2fd501ffeea8
masonrware <86129546+masonrware@users.noreply.github.com>
2022-01-09 22:22:47 -0800

    Update brain-bot.py
    rescaling and restarting

--- brain-bot.py
+++ brain-bot.py
@@ -1,17 +1,15 @@
+import asyncio
 import discord
 import os
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import youtube_dl
 
-from random import choice
 
 client = commands.Bot(command_prefix='?')
 
 
-@client.event
-async def on_ready():
-    print('brainBot is now online!')
+
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
